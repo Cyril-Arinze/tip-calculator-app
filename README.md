3086a743549de34e4b6314369b31027c1f9ae42f
Cyril Arinze <ejicyrilarinze@gmail.com>
2022-07-30 19:53:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![](screenshots/127.0.0.1_5500_index.html%20(1).png)
+![](screenshots/127.0.0.1_5500_index.html.png)
+
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- css grid
+- Mobile-first workflow
+
+
---
+++ main.js
@@ -0,0 +1,108 @@
+(function () {
+  "use strict";
+    
+  const tipButtons = document.querySelectorAll(".input2>button");
+
+  const tipPerPerson = document.querySelector(".Tip-person");
+
+  const totalPerPerson = document.querySelector(".Total-person");
+
+  const resetBtn = document.querySelector(".Reset-btn");
+
+  const errorMsg1 = document.querySelector(".error-msg1");
+
+  const errorMsg2 = document.querySelector(".error-msg2");
+
+  const customValue = document.querySelector("#custom");
+
+  function tipCalculator(percentage) {
+    const bill = parseFloat(document.querySelector("#bill").value);
+
+    const numberOfPeople = parseInt(
+      document.querySelector("#Number-people").value
+    );
+
+    if (bill && numberOfPeople) {
+      errorMsg1.classList.add("hidden");
+      errorMsg2.classList.add("hidden");
+
+      const tipPerPersonCal = (
+        ((percentage / 100) * bill) /
+        numberOfPeople
+      ).toFixed(2);
+
+      const totalPerPersonCal = (
+        bill / numberOfPeople +
+        ((percentage / 100) * bill) / numberOfPeople
+      ).toFixed(2);
+
+      tipPerPerson.innerHTML = `$${tipPerPersonCal}`;
+      totalPerPerson.innerHTML = `$${totalPerPersonCal}`;
+
+      resetBtn.removeAttribute("disabled");
+
+      resetBtn.addEventListener("click", function () {
+        tipPerPerson.innerHTML = `$0.00`;
+        totalPerPerson.innerHTML = `$0.00`;
+
+        
+        if (tipPerPerson.innerHTML === `$0.00`) {
+          resetBtn.setAttribute("disabled", "disabled");
+        }
+
+        for (let i = 0; i < tipButtons.length; i++) {
+            tipButtons[i].classList.remove("active");
+            
+        }
+      });
+    } else if (bill) {
+      errorMsg2.classList.remove("hidden");
+      errorMsg1.classList.add("hidden");
+    } else if (numberOfPeople) {
+      errorMsg1.classList.remove("hidden");
+      errorMsg2.classList.add("hidden");
+    } else {
+      errorMsg1.classList.remove("hidden");
+      errorMsg2.classList.remove("hidden");
+    }
+  }
+
+  customValue.addEventListener("keydown", function (evt) {
+    if (evt.key === "Enter") {
+
+      const customValue = parseFloat(document.querySelector("#custom").value);
+
+        for (let i = 0; i < tipButtons.length; i++) {
+            tipButtons[i].classList.remove("active");
+            
+        }
+
+      if (customValue) {
+
+        tipCalculator(customValue);
+
+
+      }
+      else{
+
+        alert("enter a valid number");
+
+      }
+    }
+  });
+  tipButtons.forEach((tipButton) => {
+    
+    tipButton.addEventListener("click", function () {
+        for (let i = 0; i < tipButtons.length; i++) {
+            tipButtons[i].classList.remove("active");
+            
+        }
+        
+        this.classList.add("active")
+        const tipPercentage = this.children[0].innerHTML;
+
+        tipCalculator(tipPercentage);
+
+    });
+  });
+})();
